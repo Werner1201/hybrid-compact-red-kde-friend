7321fbe2f4edcd5067b78d08c1269b5c5fd5746b
dirn <dirn.intg@gmail.com>
2020-10-17 06:50:20 +0800

    - update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-rm -R ~/.config/conky/hybrid-compact/
-echo 'hybrid-compact uninstalled'
+if [ -d ~/.config/conky/hybrid-compact/ ]
+then
+    rm -R ~/.config/conky/hybrid-compact/
+    echo 'hybrid-compact uninstalled'
+fi
 
 rsync -IrW --stats --exclude={'.git','deploy.sh','readme.md','screenshots'} $(pwd) ~/.config/conky/
 echo 'hybrid-compact installed'
